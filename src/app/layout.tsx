355234a9cb867a46088fdb982b44583cadf94899
junho.cho <junho.cho@naverlabs.com>
2023-08-05 15:58:43 +0900

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "삐약이 탐험대",
+  description: "삐약이 탐험대: 전세 사기에 살아남기",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
